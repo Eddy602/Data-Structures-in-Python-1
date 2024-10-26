261a4f8c9076b1399a04c4e72954d79f3f0b879e
Eddy602 <edwardodjidjaaa@gmail.com>
2024-10-26 15:53:25 +0000

    added activity 2

---
+++ main.py
@@ -0,0 +1,21 @@
+       #activity 1    
+# myarr=["red","blue","purple","white","yellow","black"]
+# print(len(myarr))
+# print(myarr[-1])
+# print(myarr[0])
+# print(myarr[2],myarr[4],"are my favourite colors")
+
+# myarr.append("brown")
+# myarr.remove("purple")
+# myarr.pop(0)
+# myarr.sort()
+# myarr.reverse()
+# myarr=myarr[1:4]
+# print(myarr)
+   #activity 2
+# mydict={1:"yellow",2:"blue",3:"red",4:"orange"}
+dict={"name":"edward","age":"13"}
+print(dict)
+dict=["address"]="accra"
+dict={"age"}=445
+dict.pop(0)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+mydict={1:"yellow",2:"blue",3:"red",4:"orange"}
